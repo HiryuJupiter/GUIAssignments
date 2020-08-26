bc8cdf6035d23cc5fba08e1e1d7400ab606096c6
HiryuJupiter <xooase@gmail.com>
2020-08-26 11:07:37 +1000

    reorganize folders

---
+++ Assets/UnityCert4/Code/StringManipulation/StringManipulation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Code.StringManipulation
+{
+    public class StringManipulation : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            string s = "MrMillsMr";
+            s = s.Replace("Mr", "James");
+            Debug.Log(s);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
